cbecf4d838e1ce3af79365c069041c615c275789
andrew <an.ngdinh@gmail.com>
2025-03-24 17:54:05 +0700

    🚀 chatbot-supervisor

--- chatbot-supervisor.py
+++ chatbot-supervisor.py
@@ -1,104 +1,154 @@
-from dotenv import load_dotenv
+from dotenv import load_dotenv, find_dotenv
 import os
-_ = load_dotenv(override=True)
-
-from typing import Annotated
-
-from typing_extensions import TypedDict
-
-from langgraph.graph import StateGraph, START, END
-from langgraph.graph.message import add_messages
-
-
-class State(TypedDict):
-    # Messages have the type "list". The `add_messages` function
-    # in the annotation defines how this state key should be updated
-    # (in this case, it appends messages to the list, rather than overwriting them)
-    messages: Annotated[list, add_messages]
-
-
-graph_builder = StateGraph(State)
+_ = load_dotenv(override=True)  # read local .env file
 
 ################################################################
+# Tools
 
-# from langchain_anthropic import ChatAnthropic
-# llm = ChatAnthropic(model="claude-3-5-sonnet-20240620")
-# from langchain_openai import ChatOpenAI
-# llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
-from langchain_google_genai import ChatGoogleGenerativeAI
-llm = ChatGoogleGenerativeAI(
-    model="gemini-1.5-flash",
-    api_key=os.getenv('GEMINI_API_KEY'),
-    temperature=0
-)
-
-################################################################
+from langchain_core.tools import tool
 
 from langchain_community.tools.tavily_search import TavilySearchResults
-tool = TavilySearchResults(max_results=2)
-tools = [tool]
-llm_with_tools = llm.bind_tools(tools)
+tavily_tool = TavilySearchResults(max_results=5)
 
-################################################################
-
-def chatbot(state: State):
-    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+from langchain_community.tools import ShellTool
+shell_tool = ShellTool()
 
 
-# The first argument is the unique node name
-# The second argument is the function or object that will be called whenever
-# the node is used.
-graph_builder.add_node("chatbot", chatbot)
+from typing import Annotated
+from langchain_experimental.utilities import PythonREPL
+repl = PythonREPL()
 
-
-graph_builder.add_edge(START, "chatbot")
-
-from langgraph.prebuilt import ToolNode, tools_condition
-tool_node = ToolNode(tools=[tool])
-graph_builder.add_node("tools", tool_node)
-
-graph_builder.add_conditional_edges(
-    "chatbot",
-    tools_condition, END
-)
-# Any time a tool is called, we return to the chatbot to decide the next step
-graph_builder.add_edge("tools", "chatbot")
-
-################################################################
-
-from langgraph.checkpoint.memory import MemorySaver
-memory = MemorySaver()
-graph = graph_builder.compile(checkpointer=memory)
-
-
-from IPython.display import Image, display
-try:
-    display(Image(graph.get_graph().draw_mermaid_png()))
-except Exception:
-    # This requires some extra dependencies and is optional
-    pass
-
-################################################################
-
-def stream_graph_updates(user_input: str):
-    for event in graph.stream(
-        {"messages": [{"role": "user", "content": user_input}]},
-        {"configurable": {"thread_id": "1"}}):
-        for value in event.values():
-            print("Assistant:", value["messages"][-1].content)
-
-
-while True:
+@tool
+def python_repl_tool(
+    code: Annotated[str, "The python code to execute to generate your chart."],
+):
+    """Use this to execute python code and do math. If you want to see the output of a value,
+    you should print it out with `print(...)`. This is visible to the user."""
+    
+    print(f"Executing code: {code}")
     try:
-        user_input = input("User: ")
-        if user_input.lower() in ["quit", "exit", "q"]:
-            print("Goodbye!")
-            break
+        result = repl.run(code)
+    except BaseException as e:
+        return f"Failed to execute. Error: {repr(e)}"
+    result_str = f"Successfully executed:\n```python\n{code}\n```\nStdout: {result}"
+    return result_str
 
-        stream_graph_updates(user_input)
-    except:
-        # fallback if input() is not available
-        user_input = "What do you know about LangGraph?"
-        print("User: " + user_input)
-        stream_graph_updates(user_input)
-        break
+################################################################
+# LLM
+
+from langchain_google_genai import ChatGoogleGenerativeAI
+llm = ChatGoogleGenerativeAI(
+    model="gemini-1.5-pro",
+    api_key=os.getenv('GEMINI_API_KEY'),
+    temperature=0,
+)
+    
+################################################################
+# Supervisor
+
+from typing import Literal
+from typing_extensions import TypedDict
+from langgraph.graph import MessagesState, END
+from langgraph.types import Command
+
+members = ["researcher", "calculator"]
+# Our team supervisor is an LLM node. It just picks the next agent to process
+# and decides when the work is completed
+options = members + ["FINISH"]
+
+system_prompt = (
+    "You are a supervisor tasked with managing a conversation between the"
+    f" following workers: {members}. Given the following user request,"
+    " respond with the worker to act next. Each worker will perform a"
+    " task and respond with their results and status. When finished,"
+    " respond with FINISH."
+)
+
+
+class Router(TypedDict):
+    """Worker to route to next. If no workers needed, route to FINISH."""
+
+    next: Literal[*options]
+    
+class State(MessagesState):
+    next: str
+
+
+def supervisor_node(state: State) -> Command[Literal[*members, "__end__"]]:
+    messages = [
+        {"role": "system", "content": system_prompt},
+    ] + state["messages"]
+    response = llm.with_structured_output(Router).invoke(messages, {"recursion_limit": 5})
+    print("-----------------", response)
+    goto = response["next"]
+    if goto == "FINISH":
+        goto = END
+
+    return Command(goto=goto, update={"next": goto})
+
+################################################################
+# Graph
+
+from langchain_core.messages import HumanMessage
+from langgraph.graph import StateGraph, START, END
+from langgraph.prebuilt import create_react_agent
+
+
+research_agent = create_react_agent(
+    llm, tools=[tavily_tool], prompt="You are a researcher. DO NOT do any math."
+)
+
+
+def research_node(state: State) -> Command[Literal["supervisor"]]:
+    result = research_agent.invoke(state)
+    return Command(
+        update={
+            "messages": [
+                HumanMessage(content=result["messages"][-1].content, name="researcher")
+            ]
+        },
+        goto="supervisor",
+    )
+
+
+# NOTE: THIS PERFORMS ARBITRARY CODE EXECUTION, WHICH CAN BE UNSAFE WHEN NOT SANDBOXED
+code_agent = create_react_agent(
+    llm, tools=[python_repl_tool], prompt="You are a calculator. You can execute math."
+)
+
+
+def code_node(state: State) -> Command[Literal["supervisor"]]:
+    result = code_agent.invoke(state)
+    return Command(
+        update={
+            "messages": [
+                HumanMessage(content=result["messages"][-1].content, name="calculator")
+            ]
+        },
+        goto="supervisor",
+    )
+
+
+builder = StateGraph(State)
+builder.add_edge(START, "supervisor")
+builder.add_node("supervisor", supervisor_node)
+builder.add_node("researcher", research_node)
+builder.add_node("calculator", code_node)
+graph = builder.compile()
+
+from IPython.display import display, Image
+display(Image(graph.get_graph().draw_png()))
+
+
+################################################################
+
+for s in graph.stream(
+    {"messages": [("user",
+                # "Average of 21 and 49", # MALFORMED_FUNCTION_CALL
+                # "GDP of New York and California",
+                #    "What is average GDP of New York and California?",
+                   "How many games have Arsenal played in EPL season 2024-2025? How many are left?",
+                   )]}, subgraphs=True
+):
+    print(s)
+    print("----")
