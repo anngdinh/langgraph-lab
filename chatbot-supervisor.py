f4c8782e5eec19ee4c9bbeaa06247ad568caedfb
andrew <an.ngdinh@gmail.com>
2025-03-24 16:23:12 +0700

    🚀 init chatbot-supervisor

---
+++ chatbot-supervisor.py
@@ -0,0 +1,104 @@
+from dotenv import load_dotenv
+import os
+_ = load_dotenv(override=True)
+
+from typing import Annotated
+
+from typing_extensions import TypedDict
+
+from langgraph.graph import StateGraph, START, END
+from langgraph.graph.message import add_messages
+
+
+class State(TypedDict):
+    # Messages have the type "list". The `add_messages` function
+    # in the annotation defines how this state key should be updated
+    # (in this case, it appends messages to the list, rather than overwriting them)
+    messages: Annotated[list, add_messages]
+
+
+graph_builder = StateGraph(State)
+
+################################################################
+
+# from langchain_anthropic import ChatAnthropic
+# llm = ChatAnthropic(model="claude-3-5-sonnet-20240620")
+# from langchain_openai import ChatOpenAI
+# llm = ChatOpenAI(model="gpt-3.5-turbo", temperature=0)
+from langchain_google_genai import ChatGoogleGenerativeAI
+llm = ChatGoogleGenerativeAI(
+    model="gemini-1.5-flash",
+    api_key=os.getenv('GEMINI_API_KEY'),
+    temperature=0
+)
+
+################################################################
+
+from langchain_community.tools.tavily_search import TavilySearchResults
+tool = TavilySearchResults(max_results=2)
+tools = [tool]
+llm_with_tools = llm.bind_tools(tools)
+
+################################################################
+
+def chatbot(state: State):
+    return {"messages": [llm_with_tools.invoke(state["messages"])]}
+
+
+# The first argument is the unique node name
+# The second argument is the function or object that will be called whenever
+# the node is used.
+graph_builder.add_node("chatbot", chatbot)
+
+
+graph_builder.add_edge(START, "chatbot")
+
+from langgraph.prebuilt import ToolNode, tools_condition
+tool_node = ToolNode(tools=[tool])
+graph_builder.add_node("tools", tool_node)
+
+graph_builder.add_conditional_edges(
+    "chatbot",
+    tools_condition, END
+)
+# Any time a tool is called, we return to the chatbot to decide the next step
+graph_builder.add_edge("tools", "chatbot")
+
+################################################################
+
+from langgraph.checkpoint.memory import MemorySaver
+memory = MemorySaver()
+graph = graph_builder.compile(checkpointer=memory)
+
+
+from IPython.display import Image, display
+try:
+    display(Image(graph.get_graph().draw_mermaid_png()))
+except Exception:
+    # This requires some extra dependencies and is optional
+    pass
+
+################################################################
+
+def stream_graph_updates(user_input: str):
+    for event in graph.stream(
+        {"messages": [{"role": "user", "content": user_input}]},
+        {"configurable": {"thread_id": "1"}}):
+        for value in event.values():
+            print("Assistant:", value["messages"][-1].content)
+
+
+while True:
+    try:
+        user_input = input("User: ")
+        if user_input.lower() in ["quit", "exit", "q"]:
+            print("Goodbye!")
+            break
+
+        stream_graph_updates(user_input)
+    except:
+        # fallback if input() is not available
+        user_input = "What do you know about LangGraph?"
+        print("User: " + user_input)
+        stream_graph_updates(user_input)
+        break
